9e1869ec2a1bb4a33c09d568817d92a7aeac5ce3
Ivan <ivan.venelinov.shulev@gmail.com>
2019-10-12 18:37:29 +0300

    remove unused elements

--- header.php
+++ header.php
@@ -22,7 +22,7 @@
 
 <body <?php body_class(); ?>>
 <div id="page" class="site container">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'cami' ); ?></a>
+	<a class="skip-link hidden screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'cami' ); ?></a>
 
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
@@ -64,32 +64,8 @@
 					) );
 				?>
 			</nav>
-			<?php
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$cami_description = get_bloginfo( 'description', 'display' );
-			if ( $cami_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $cami_description; /* WPCS: xss ok. */ ?></p>
-			<?php endif; ?>
 		</div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'cami' ); ?></button>
-			<?php
-			wp_nav_menu( array(
-				'theme_location' => 'menu-1',
-				'menu_id'        => 'primary-menu',
-			) );
-			?>
-		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
