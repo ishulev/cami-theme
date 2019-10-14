2bafd85d50e88003f1a97d19965cc5a0dc951447
Ivan <ivan.venelinov.shulev@gmail.com>
2019-10-14 19:03:17 +0300

    add home link to logo

--- header.php
+++ header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The header for our theme
  *
@@ -12,8 +13,9 @@
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
+
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
@@ -21,27 +23,27 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="site container">
-	<a hidden class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'cami' ); ?></a>
+	<div id="page" class="site container">
+		<a hidden class="skip-link screen-reader-text" href="#content"><?php esc_html_e('Skip to content', 'cami'); ?></a>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<nav class="navbar navbar-light navbar-expand-lg">
-				<a class="navbar-brand" href="#">
-				<?php
-					$custom_logo_id = get_theme_mod( 'custom_logo' );
-					$logo = wp_get_attachment_image_src( $custom_logo_id , 'full' );
-					if ( has_custom_logo() ) {
-							echo '<img width="200px" src="' . esc_url( $logo[0] ) . '" alt="' . get_bloginfo( 'name' ) . '">';
-					} else {
-							echo '<h1>'. get_bloginfo( 'name' ) .'</h1>';
-					}
-				?>
-				</a>
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
-				</button>
-				<!-- <div class="collapse navbar-collapse" id="navbarNavDropdown">
+		<header id="masthead" class="site-header">
+			<div class="site-branding">
+				<nav class="navbar navbar-light navbar-expand-lg">
+					<a class="navbar-brand" href="<?php echo home_url() ?>">
+						<?php
+						$custom_logo_id = get_theme_mod('custom_logo');
+						$logo = wp_get_attachment_image_src($custom_logo_id, 'full'); ?>
+						<?php if (has_custom_logo()) :
+							echo '<img width="200px" src="' . esc_url($logo[0]) . '" alt="' . get_bloginfo('name') . '">';
+						else :
+							echo '<h1>' . get_bloginfo('name') . '</h1>';
+						endif
+						?>
+					</a>
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+					<!-- <div class="collapse navbar-collapse" id="navbarNavDropdown">
 					<ul class="navbar-nav">
 						<li class="nav-item active">
 							<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
@@ -54,18 +56,18 @@
 						</li>
 					</ul>
 				</div> -->
-				<?php
-					wp_nav_menu( array(
+					<?php
+					wp_nav_menu(array(
 						'theme_location' => 'menu-1',
 						'menu_id'        => 'primary-menu',
-						'container_class'=> 'collapse navbar-collapse',
+						'container_class' => 'collapse navbar-collapse',
 						'container_id'	 => 'navbarNavDropdown',
 						'menu_class'	 => 'navbar-nav ml-auto'
-					) );
-				?>
-			</nav>
-		</div><!-- .site-branding -->
+					));
+					?>
+				</nav>
+			</div><!-- .site-branding -->
 
-	</header><!-- #masthead -->
+		</header><!-- #masthead -->
 
-	<div id="content" class="site-content">
+		<div id="content" class="site-content">
