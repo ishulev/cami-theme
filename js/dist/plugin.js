23a026f99407c80838758b37d9c7c26829cc63b8
Ivan Shulev <ivan.venelinov.shulev@gmail.com>
2019-10-29 10:46:49 +0200

    add signed rules for quantity and priceclear

--- js/dist/plugin.js
+++ js/dist/plugin.js
@@ -98,69 +98,67 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _wordpress_element__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @wordpress/element */ "@wordpress/element");
 /* harmony import */ var _wordpress_element__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_wordpress_element__WEBPACK_IMPORTED_MODULE_0__);
 
+var registerPlugin = wp.plugins.registerPlugin;
+var PluginSidebar = wp.editPost.PluginSidebar;
+var __ = wp.i18n.__;
+var el = wp.element.createElement;
+var Text = wp.components.TextControl;
+var FormFileUpload = wp.components.FormFileUpload;
+var withSelect = wp.data.withSelect;
+var withDispatch = wp.data.withDispatch;
 
-(function (wp) {
-  var registerPlugin = wp.plugins.registerPlugin;
-  var PluginSidebar = wp.editPost.PluginSidebar;
-  var el = wp.element.createElement;
-  var Text = wp.components.TextControl;
-  var FormFileUpload = wp.components.FormFileUpload;
-  var withSelect = wp.data.withSelect;
-  var withDispatch = wp.data.withDispatch;
+var MyFormFileUpload = function MyFormFileUpload() {
+  return Object(_wordpress_element__WEBPACK_IMPORTED_MODULE_0__["createElement"])(FormFileUpload, {
+    accept: "image/*",
+    onChange: function onChange() {
+      return console.log('new image');
+    }
+  }, "Upload");
+};
 
-  var MyFormFileUpload = function MyFormFileUpload() {
-    return Object(_wordpress_element__WEBPACK_IMPORTED_MODULE_0__["createElement"])(FormFileUpload, {
-      accept: "image/*",
-      onChange: function onChange() {
-        return console.log('new image');
-      }
-    }, "Upload");
+var mapSelectToProps = function mapSelectToProps(select) {
+  return {
+    metaFieldValue: select('core/editor').getEditedPostAttribute('meta')['sidebar_plugin_meta_block_field']
   };
+};
 
-  var mapSelectToProps = function mapSelectToProps(select) {
-    return {
-      metaFieldValue: select('core/editor').getEditedPostAttribute('meta')['sidebar_plugin_meta_block_field']
-    };
+var mapDispatchToProps = function mapDispatchToProps(dispatch) {
+  return {
+    setMetaFieldValue: function setMetaFieldValue(value) {
+      dispatch('core/editor').editPost({
+        meta: {
+          sidebar_plugin_meta_block_field: value
+        }
+      });
+    }
   };
+};
 
-  var mapDispatchToProps = function mapDispatchToProps(dispatch) {
-    return {
-      setMetaFieldValue: function setMetaFieldValue(value) {
-        dispatch('core/editor').editPost({
-          meta: {
-            sidebar_plugin_meta_block_field: value
-          }
-        });
-      }
-    };
-  };
-
-  var MetaBlockField = function MetaBlockField(props) {
-    return el(FormFileUpload, {
-      label: 'Meta Block Field',
-      value: props.metaFieldValue,
-      accept: 'image/*',
-      multiple: true,
-      onChange: function onChange(content) {
-        console.log('New image => ', content); // props.setMetaFieldValue(content);
-      }
-    });
-  };
-
-  var MetaBlockFieldWithData = withSelect(mapSelectToProps)(MetaBlockField);
-  var MetaBlockFieldWithDataAndActions = withDispatch(mapDispatchToProps)(MetaBlockFieldWithData);
-  registerPlugin('my-plugin-sidebar', {
-    render: function render() {
-      return el(PluginSidebar, {
-        name: 'my-plugin-sidebar',
-        icon: 'admin-post',
-        title: 'My plugin sidebar'
-      }, el('div', {
-        className: 'plugin-sidebar-content'
-      }, el(MetaBlockFieldWithDataAndActions)));
+var MetaBlockField = function MetaBlockField(props) {
+  return el(FormFileUpload, {
+    label: 'Meta Block Field',
+    value: props.metaFieldValue,
+    accept: 'image/*',
+    multiple: true,
+    onChange: function onChange(content) {
+      console.log('New image => ', content); // props.setMetaFieldValue(content);
     }
   });
-})(window.wp);
+};
+
+var MetaBlockFieldWithData = withSelect(mapSelectToProps)(MetaBlockField);
+var MetaBlockFieldWithDataAndActions = withDispatch(mapDispatchToProps)(MetaBlockFieldWithData);
+registerPlugin('my-plugin-sidebar', {
+  render: function render() {
+    return el(PluginSidebar, {
+      name: 'my-plugin-sidebar',
+      icon: 'admin-post',
+      title: 'My plugin sidebar'
+    }, el('div', {
+      className: 'plugin-sidebar-content'
+    }, el(MetaBlockFieldWithDataAndActions)));
+  }
+});
 
 /***/ }),
 
--- js/plugin.js
+++ js/plugin.js
@@ -1,68 +1,67 @@
-(function (wp) {
-  var registerPlugin = wp.plugins.registerPlugin;
-  var PluginSidebar = wp.editPost.PluginSidebar;
-  var el = wp.element.createElement;
-  var Text = wp.components.TextControl;
-  var FormFileUpload = wp.components.FormFileUpload;
-  var withSelect = wp.data.withSelect;
-  var withDispatch = wp.data.withDispatch;
- 
+const { registerPlugin } = wp.plugins;
+const { PluginSidebar } = wp.editPost;
+const { __ } = wp.i18n;
+var el = wp.element.createElement;
+var Text = wp.components.TextControl;
+var FormFileUpload = wp.components.FormFileUpload;
+var withSelect = wp.data.withSelect;
+var withDispatch = wp.data.withDispatch;
+
 const MyFormFileUpload = () => (
-    <FormFileUpload
-        accept="image/*"
-        onChange={ () => console.log('new image') }
-    >
-        Upload
+  <FormFileUpload
+    accept="image/*"
+    onChange={() => console.log('new image')}
+  >
+    Upload
     </FormFileUpload>
 );
 
-  var mapSelectToProps = function (select) {
-    return {
-      metaFieldValue: select('core/editor')
-        .getEditedPostAttribute('meta')
-      ['sidebar_plugin_meta_block_field'],
-    }
+var mapSelectToProps = function (select) {
+  return {
+    metaFieldValue: select('core/editor')
+      .getEditedPostAttribute('meta')
+    ['sidebar_plugin_meta_block_field'],
   }
+}
 
-  var mapDispatchToProps = function (dispatch) {
-    return {
-      setMetaFieldValue: function (value) {
-        dispatch('core/editor').editPost(
-          { meta: { sidebar_plugin_meta_block_field: value } }
-        );
-      }
-    }
-  }
-
-  var MetaBlockField = function (props) {
-    return el(FormFileUpload, {
-      label: 'Meta Block Field',
-      value: props.metaFieldValue,
-      accept: 'image/*',
-      multiple: true,
-      onChange: function (content) {
-        console.log('New image => ', content)
-        // props.setMetaFieldValue(content);
-      },
-    });
-  }
-
-  var MetaBlockFieldWithData = withSelect(mapSelectToProps)(MetaBlockField);
-  var MetaBlockFieldWithDataAndActions = withDispatch(mapDispatchToProps)(MetaBlockFieldWithData);
-
-  registerPlugin('my-plugin-sidebar', {
-    render: function () {
-      return el(PluginSidebar,
-        {
-          name: 'my-plugin-sidebar',
-          icon: 'admin-post',
-          title: 'My plugin sidebar',
-        },
-        el('div',
-          { className: 'plugin-sidebar-content' },
-          el(MetaBlockFieldWithDataAndActions)
-        )
+var mapDispatchToProps = function (dispatch) {
+  return {
+    setMetaFieldValue: function (value) {
+      dispatch('core/editor').editPost(
+        { meta: { sidebar_plugin_meta_block_field: value } }
       );
     }
+  }
+}
+
+var MetaBlockField = function (props) {
+  return el(FormFileUpload, {
+    label: 'Meta Block Field',
+    value: props.metaFieldValue,
+    accept: 'image/*',
+    multiple: true,
+    onChange: function (content) {
+      console.log('New image => ', content)
+      // props.setMetaFieldValue(content);
+    },
   });
-})(window.wp);
+}
+
+var MetaBlockFieldWithData = withSelect(mapSelectToProps)(MetaBlockField);
+var MetaBlockFieldWithDataAndActions = withDispatch(mapDispatchToProps)(MetaBlockFieldWithData);
+
+registerPlugin('my-plugin-sidebar', {
+  render: function () {
+    return el(PluginSidebar,
+      {
+        name: 'my-plugin-sidebar',
+        icon: 'admin-post',
+        title: 'My plugin sidebar',
+      },
+      el('div',
+        { className: 'plugin-sidebar-content' },
+        el(MetaBlockFieldWithDataAndActions)
+      )
+    );
+  }
+});
