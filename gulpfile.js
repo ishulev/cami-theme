94756e9766a3baafe5eee9c642097badbbb1e863
Ivan Shulev <ivan.venelinov.shulev@gmail.com>
2019-10-29 10:41:47 +0200

    update to use wp scripts and remove unnecessary packages

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var babel = require('gulp-babel');
 
 sass.compiler = require('node-sass');
 
@@ -12,25 +11,8 @@ gulp.task('sass', function () {
         .pipe(gulp.dest('./'));
 });
 
-gulp.task('js', function () {
-    return gulp.src('js/plugin.js')
-        .pipe(babel({
-            presets: ['@babel/preset-env', '@babel/preset-react'],
-            plugins: ['@babel/plugin-transform-modules-commonjs']
-        }))
-        .on('error', function (e) {
-            console.error(e);
-            // this.emit('end');
-        })
-        .pipe(gulp.dest('js/dist'))
-});
-
 gulp.task('sass:watch', function () {
     gulp.watch('./sass/**/*.scss', ['sass']);
 });
 
-gulp.task('js:watch', function () {
-    gulp.watch('./js/plugin.js', ['js']);
-});
-
-gulp.task('default', ['sass', 'js', 'sass:watch', 'js:watch']);
+gulp.task('default', ['sass', 'sass:watch']);
