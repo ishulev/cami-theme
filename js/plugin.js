7fcafaa0e4cdc3d2685a2b5a7fc304ee673faa28
Ivan Shulev <ivan.venelinov.shulev@gmail.com>
2019-10-30 11:06:05 +0200

    upload files to media library

--- js/plugin.js
+++ js/plugin.js
@@ -1,14 +1,22 @@
 import { registerPlugin } from '@wordpress/plugins';
 import { PluginDocumentSettingPanel } from '@wordpress/edit-post';
 import { FormFileUpload } from '@wordpress/components';
+import { uploadMedia } from '@wordpress/media-utils';
 import { __ } from '@wordpress/i18n';
+
+const handleFileChange = returnedFiles => console.log(returnedFiles);
+const handleFileError = console.error;
 
 const MyDocumentSettingTest = () => (
   <PluginDocumentSettingPanel className="my-document-setting-plugin" title="Добави медец">
     <FormFileUpload
       accept="image/*"
       multiple
-      onChange={ev => console.log('new image => ', ev.target.files)}
+      onChange={ev => uploadMedia({
+        filesList: ev.target.files,
+        onFileChange: handleFileChange,
+        onError: handleFileError
+      })}
     >
       Upload
     </FormFileUpload>
