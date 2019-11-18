6f43604b85f9f7575a50ec401c271523193341d9
Ivan Shulev <ivan.venelinov.shulev@gmail.com>
2019-11-18 17:06:20 +0200

    print associated image urls to post

--- template-parts/content-post.php
+++ template-parts/content-post.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Template part for displaying posts
  *
@@ -11,11 +12,16 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php
-		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
+		<h1><?php $associatedMedia = get_post_meta(get_the_ID(), 'sidebar_plugin_meta_block_field');
+			print_r($associatedMedia); ?></h1>
+		<?php foreach ($associatedMedia as $med) {
+			echo wp_get_attachment_image_src($med)[0];
+		} ?>
+
+		<?php if ( is_singular() ) :
+		the_title( '<h1 class="entry-title">', '</h1>' );
 		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+		the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		endif; ?>
 	</header><!-- .entry-header -->
 
@@ -23,10 +29,10 @@
 
 	<div class="entry-content">
 		<?php
-		the_content( sprintf(
+		the_content(sprintf(
 			wp_kses(
 				/* translators: %s: Name of current post. Only visible to screen readers */
-				__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'cami' ),
+				__('Continue reading<span class="screen-reader-text"> "%s"</span>', 'cami'),
 				array(
 					'span' => array(
 						'class' => array(),
@@ -34,12 +40,12 @@
 				)
 			),
 			get_the_title()
-		) );
+		));
 
-		wp_link_pages( array(
-			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'cami' ),
+		wp_link_pages(array(
+			'before' => '<div class="page-links">' . esc_html__('Pages:', 'cami'),
 			'after'  => '</div>',
-		) );
+		));
 		?>
 	</div><!-- .entry-content -->
 </article><!-- #post-<?php the_ID(); ?> -->
