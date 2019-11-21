6557be0faf3f23279e1bd90d8ddded387e89ecb6
Ivan Shulev <ivan.venelinov.shulev@gmail.com>
2019-11-21 13:03:55 +0200

    final touches

--- template-parts/content-post.php
+++ template-parts/content-post.php
@@ -9,35 +9,9 @@
  */
 
 ?>
-
+<?php $associatedMedia = get_post_meta(get_the_ID(), 'sidebar_plugin_meta_block_field'); ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<h1><?php $associatedMedia = get_post_meta(get_the_ID(), 'sidebar_plugin_meta_block_field');
-			print_r($associatedMedia); ?></h1>
-
-		<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-			<ol class="carousel-indicators">
-				<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-				<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-				<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-			</ol>
-			<div class="carousel-inner">
-				<?php foreach ($associatedMedia as $key => $med) { ?>
-					<div class="carousel-item<?php echo !$key ? ' active' : '' ?>">
-						<img src="<?php echo wp_get_attachment_image_src($med)[0]; ?>" class="d-block w-100" alt="...">
-					</div>
-				<?php } ?>
-			</div>
-			<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-				<span class="sr-only">Previous</span>
-			</a>
-			<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-				<span class="carousel-control-next-icon" aria-hidden="true"></span>
-				<span class="sr-only">Next</span>
-			</a>
-		</div>
-
 		<?php if (is_singular()) :
 			the_title('<h1 class="entry-title">', '</h1>');
 		else :
@@ -50,24 +24,50 @@
 	?>
 
 	<div class="entry-content">
-		<?php
-		the_content(sprintf(
-			wp_kses(
-				/* translators: %s: Name of current post. Only visible to screen readers */
-				__('Continue reading<span class="screen-reader-text"> "%s"</span>', 'cami'),
-				array(
-					'span' => array(
-						'class' => array(),
+		<div class="row">
+			<div class="col-4">
+				<?php
+				the_content(sprintf(
+					wp_kses(
+						/* translators: %s: Name of current post. Only visible to screen readers */
+						__('Continue reading<span class="screen-reader-text"> "%s"</span>', 'cami'),
+						array(
+							'span' => array(
+								'class' => array(),
+							),
+						)
 					),
-				)
-			),
-			get_the_title()
-		));
+					get_the_title()
+				));
 
-		wp_link_pages(array(
-			'before' => '<div class="page-links">' . esc_html__('Pages:', 'cami'),
-			'after'  => '</div>',
-		));
-		?>
+				wp_link_pages(array(
+					'before' => '<div class="page-links">' . esc_html__('Pages:', 'cami'),
+					'after'  => '</div>',
+				));
+				?>
+			</div>
+			<div class="col">
+				<div id="project-images_carousel" style="background-color: burlywood" class="carousel slide" data-ride="carousel">
+					<ol class="carousel-indicators">
+						<li data-target="#project-images_carousel" data-slide-to="0" class="active"></li>
+						<li data-target="#project-images_carousel" data-slide-to="1"></li>
+						<li data-target="#project-images_carousel" data-slide-to="2"></li>
+					</ol>
+					<div class="carousel-inner">
+						<?php foreach ($associatedMedia as $key => $med) { ?>
+							<div class="carousel-item<?php echo !$key ? ' active' : '' ?>">
+								<img src="<?php echo wp_get_attachment_image_src($med, 'original')[0]; ?>" class="d-block w-100 img-fluid" alt="...">
+							</div>
+						<?php } ?>
+					</div>
+					<a class="carousel-control-prev" href="#project-images_carousel" role="button" data-slide="prev">
+						<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+					</a>
+					<a class="carousel-control-next" href="#project-images_carousel" role="button" data-slide="next">
+						<span class="carousel-control-next-icon" aria-hidden="true"></span>
+					</a>
+				</div>
+			</div>
+		</div>
 	</div><!-- .entry-content -->
 </article><!-- #post-<?php the_ID(); ?> -->
